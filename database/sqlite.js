80cff4c6b6646934238a9897ea476bdacd7eefa3
Daniel Maziad <dmaziad@gmail.com>
2019-02-08 11:46:18 -0500

    Added get, post, and patch requests to server

--- database/sqlite.js
+++ database/sqlite.js
@@ -1,18 +1,12 @@
 const sqlite3 = require('sqlite3').verbose();
-const faker = require('faker');
 const generator = require('./generator');
 
-let db = new sqlite3.Database('./database/reviewdb.db', (err) => {
-    if (err) {
-        console.error('ERROR: There was a problem connecting to the review database.');
-    }
-    console.log('Connected to the review database!')
-});
+let db = new sqlite3.Database('./database/reviewdb.db');
 
 db.serialize(function() {
     db.run("DROP TABLE IF EXISTS reviews");
-    db.run("CREATE TABLE reviews (\
-                id INTEGER,\
+    db.run("CREATE TABLE IF NOT EXISTS reviews (\
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\
                 item_id INTEGER,\
                 title VARCHAR(50),\
                 pros VARCHAR(100),\
@@ -26,19 +20,20 @@ db.serialize(function() {
                 not_helpful INTEGER DEFAULT 0)"
                 );
 
-    let stmt = db.prepare("INSERT INTO reviews(id, item_id,title,pros,\
-    cons,body,verified,date,eggs,author) VALUES (?,?,?,?,?,?,?,?,?,?)")
+    let stmt = db.prepare("INSERT INTO reviews(item_id,title,pros,\
+    cons,body,verified,date,eggs,author) VALUES (?,?,?,?,?,?,?,?,?)")
     for (let i = 0; i < 400; i++) {
         if ((i % 3 === 0 && i > 40) || (i % 2 === 0 && i > 240)) {
-            stmt.run(i, generator.item_id(100), generator.title(),
+            stmt.run(generator.item_id(100), generator.title(),
             generator.pros(), generator.cons(), generator.body(), "F",
             generator.date(1000), generator.eggs(6),
             "Anonymous");
-        }
-        stmt.run(i, generator.item_id(100), generator.title(),
+        } else {
+        stmt.run(generator.item_id(100), generator.title(),
         generator.pros(), generator.cons(), generator.body(),
         generator.verified(), generator.date(1000),
         generator.eggs(6), generator.author())
+        }
     }
     stmt.finalize();
 
--- server/index.js
+++ server/index.js
@@ -1,35 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3')
+const sqlite3 = require('sqlite3');
 
 const app = express();
-const port = 3004;
+const port = 3009;
 const router = express.Router();
+
+let db = new sqlite3.Database('./database/reviewdb.db');
 
 app.use(express.static(__dirname + '/../client/'));
 app.use(bodyParser.json());
 app.use('/', router);
 
-    let db = new sqlite3.Database('../database/reviewdb.db', (err) => {
-        if (err) {
-            console.log('error connecting to database');
-        } else {
-            console.log('connected to database')
-        }
-    });
-router.get('/reviews', function(req, res) {
-    
+router.get('/reviews', (req, res) => {
     db.all('SELECT * FROM reviews WHERE item_id = ?', [req.body.item_id], (err, row) => {
         if (err) {
             console.error('ERROR occurred while retrieving reviews')
         }
-        if (row) {
-            console.log(row);
-        } else {
-            console.log('No reviews found for this item');
-        }
+        res.send(200, row);2
     })
 });
+
+router.post('/reviews', (req, res) => {
+    let newPost = req.body;
+    let stmt = db.prepare('INSERT INTO reviews (item_id, title, pros,\
+    cons,body,verified,date,eggs,author) VALUES (?,?,?,?,?,?,?,?,?)');
+    stmt.run(newPost.item_id, newPost.title, newPost.pros,
+    newPost.cons, newPost.body, newPost.verified, newPost.date,
+    newPost.eggs, newPost.author)
+    stmt.finalize();
+    res.send(201);
+});
+
+router.patch('/reviews', (req, res) => {
+    let newPost = req.body;
+    if (req.body.helpful === true) {
+        let stmt = db.prepare('UPDATE reviews SET helpful = helpful + 1 WHERE id = ?');
+        stmt.run(newPost.id)
+        stmt.finalize();
+        res.send(201);
+    }
+    if (req.body.helpful === false) {
+        let stmt = db.prepare('UPDATE reviews SET not_helpful = not_helpful + 1 WHERE id = ?');
+        stmt.run(newPost.id)
+        stmt.finalize();
+        res.send(201);
+    }
+})
 
 app.listen(port, () => {
     console.log('Listening on port ' + port);
