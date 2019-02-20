407f662c457a22f2c3d3382c83dd401335497220
Daniel Maziad <dmaziad@gmail.com>
2019-02-19 20:00:11 -0500

    Added rating info to overview component

--- client/src/components/overview.jsx
+++ client/src/components/overview.jsx
@@ -9,6 +9,26 @@ class Overview extends React.Component {
         return (
             <div className="review_overview">
             <h1> Customer Reviews of This Item</h1>
+            <ul>
+                <li>5 Eggs: {this.props.reviews.filter((review) => {
+                    return review.eggs === 5
+                }).length / this.props.reviews.length * 100}%</li>
+                <li>4 Eggs: {this.props.reviews.filter((review) => {
+                    return review.eggs === 4
+                }).length}/{this.props.reviews.length}</li>
+                <li>3 Eggs: {this.props.reviews.filter((review) => {
+                    return review.eggs === 3
+                }).length}/{this.props.reviews.length}</li>
+                <li>2 Eggs: {this.props.reviews.filter((review) => {
+                    return review.eggs === 2
+                }).length}/{this.props.reviews.length}</li>
+                <li>1 Eggs: {this.props.reviews.filter((review) => {
+                    return review.eggs === 1
+                }).length}/{this.props.reviews.length}</li>
+                <li>0 Eggs: {this.props.reviews.filter((review) => {
+                    return review.eggs === 0
+                }).length}/{this.props.reviews.length}</li>
+            </ul>
             </div>
         )
     }
