24190dca02f2a6af2ad0066efe1e9eda4b2fecc8
Daniel Maziad <dmaziad@gmail.com>
2019-02-21 15:32:40 -0500

    push dist folder

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../client/dist/bundle.js'))
+    res.sendFile(path.join(__dirname + '/../client/dist/index.html'))
 });
 
 app.get('/reviews/:item_id', (req, res) => {
