c728cd1ab7a11a51829365a27166f88787898d20
Daniel Maziad <dmaziad@gmail.com>
2019-02-21 15:19:50 -0500

    fixed bug in get request

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.SendFile(path.join(__dirname + '/../client/dist/index.html'))
+    res.sendFile(path.join(__dirname + '/../client/dist/index.html'))
 });
 
 app.get('/reviews/:item_id', (req, res) => {
