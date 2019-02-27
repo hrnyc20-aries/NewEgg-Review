326521fffeed6acb3bda27ff803e056020ef335f
Grahn Cooledge <gcooled29@gmail.com>
2019-02-27 18:28:31 -0500

    Added NODE_ENV Configuration For Environemt

--- database/sqlite.js
+++ database/sqlite.js
@@ -1,11 +1,14 @@
 const sqlite3 = require('sqlite3').verbose();
 const generator = require('./generator');
+const dbPath = require('../server/config').dePath;
 
-let db = new sqlite3.Database('./database/reviewdb.db');
+// let db = new sqlite3.Database('./database/reviewdb.db');
+let db = new sqlite3.Database(dbPath);
 
 db.serialize(function() {
-    db.run("DROP TABLE IF EXISTS reviews");
-    db.run("CREATE TABLE IF NOT EXISTS reviews (\
+  db.run('DROP TABLE IF EXISTS reviews');
+  db.run(
+    'CREATE TABLE IF NOT EXISTS reviews (\
                 id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\
                 item_id INTEGER,\
                 title VARCHAR(50),\
@@ -17,35 +20,66 @@ db.serialize(function() {
                 eggs INTEGER DEFAULT 0,\
                 author VARCHAR(30), \
                 helpful INTEGER NULL DEFAULT 0,\
-                not_helpful INTEGER DEFAULT 0)"
-                );
+                not_helpful INTEGER DEFAULT 0)'
+  );
 
-    let stmt = db.prepare("INSERT INTO reviews(item_id,title,pros,\
-    cons,body,verified,date,eggs,author) VALUES (?,?,?,?,?,?,?,?,?)")
-    for (let i = 0; i < 500; i++) {
-        if ((i % 3 === 0 && i > 90) || (i % 2 === 0 && i > 400)) {
-            stmt.run(generator.item_id(89), generator.title(),
-            generator.pros(), generator.cons(), generator.body(), "F",
-            generator.date(1000), generator.eggs(5),
-            "Anonymous");
-        } else {
-            stmt.run(generator.item_id(89), generator.title(),
-            generator.pros(), generator.cons(), generator.body(),
-            generator.verified(), generator.date(1000),
-            generator.eggs(5), generator.author())
-            }
+  let stmt = db.prepare(
+    'INSERT INTO reviews(item_id,title,pros,\
+    cons,body,verified,date,eggs,author) VALUES (?,?,?,?,?,?,?,?,?)'
+  );
+  for (let i = 0; i < 500; i++) {
+    if ((i % 3 === 0 && i > 90) || (i % 2 === 0 && i > 400)) {
+      stmt.run(
+        generator.item_id(89),
+        generator.title(),
+        generator.pros(),
+        generator.cons(),
+        generator.body(),
+        'F',
+        generator.date(1000),
+        generator.eggs(5),
+        'Anonymous'
+      );
+    } else {
+      stmt.run(
+        generator.item_id(89),
+        generator.title(),
+        generator.pros(),
+        generator.cons(),
+        generator.body(),
+        generator.verified(),
+        generator.date(1000),
+        generator.eggs(5),
+        generator.author()
+      );
     }
-    for (let j = 0; j < 50; j++) {
-        stmt.run(generator.item_id(99), generator.title(),
-        generator.pros(), generator.cons(), generator.body(),
-        generator.verified(), generator.date(100),
-        generator.eggs(5), generator.author())
-    }
-    stmt.finalize();
+  }
+  for (let j = 0; j < 50; j++) {
+    stmt.run(
+      generator.item_id(99),
+      generator.title(),
+      generator.pros(),
+      generator.cons(),
+      generator.body(),
+      generator.verified(),
+      generator.date(100),
+      generator.eggs(5),
+      generator.author()
+    );
+  }
+  stmt.finalize();
 
-    db.each("SELECT * FROM reviews", function(err, row) {
-        console.log('review #' + row.id + ' for item #' + row.item_id +' by: ' + row.author + row.date)
-    });
+  db.each('SELECT * FROM reviews', function(err, row) {
+    console.log(
+      'review #' +
+        row.id +
+        ' for item #' +
+        row.item_id +
+        ' by: ' +
+        row.author +
+        row.date
+    );
+  });
 });
 
 db.close();
