601b61d8e057b087f6b5f377879df06f9533cb25
Grahn Cooledge <gcooled29@gmail.com>
2019-03-01 15:07:08 -0500

    Generate Seed File -- Efficient

--- server/database/generator.js
+++ server/database/generator.js
@@ -1,14 +1,8 @@
 const faker = require('faker');
 
 let seedModel = () => {
-  // let seed = Object.create(staticObj);
-  let seed = {};
+  let seed = Object.create(staticObj);
 
-  seed.seed.title = faker.lorem.words();
-  seed.pros = faker.lorem.sentence();
-  seed.cons = faker.lorem.sentence();
-  seed.body = faker.lorem.paragraph();
-  seed.author = faker.name.findName();
   seed.item_id = Math.floor(Math.random() * process.env.MONGO_AMOUNT) + 1;
   seed.date = JSON.stringify(faker.date.recent(10000)).replace(/"/g, '');
   seed.eggs = Math.floor(Math.random() * 5) + 1;
@@ -21,13 +15,13 @@ let seedModel = () => {
   return seed;
 };
 
-// let staticObj = {
-//   title: faker.lorem.words(),
-//   pros: faker.lorem.sentence(),
-//   cons: faker.lorem.sentence(),
-//   body: faker.lorem.paragraph(),
-//   author: faker.name.findName()
-// };
+let staticObj = {
+  title: faker.lorem.words(),
+  pros: faker.lorem.sentence(),
+  cons: faker.lorem.sentence(),
+  body: faker.lorem.paragraph(),
+  author: faker.name.findName()
+};
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
