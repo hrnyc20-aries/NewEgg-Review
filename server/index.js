26c1dbecfcdde0688b0590e100448fd21eb8d2ea
Grahn Cooledge <gcooled29@gmail.com>
2019-03-04 15:34:20 -0500

    Clean Up Server File: Remove Erroneous Code

--- server/index.js
+++ server/index.js
@@ -1,48 +1,33 @@
 const path = require('path');
 const express = require('express');
+const db = require('./database');
 const compression = require('compression');
 const routes = require('./api');
 
 const cors = require('cors');
 const PORT = process.env.PORT || 3009;
 
-const app = express();
+const server = express();
 
-app.use(compression());
-app.use(express.json());
-app.use(cors());
+server.use(compression());
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(cors());
 
-app.get('*.js', function(req, res, next) {
+server.get('*.js', function(req, res, next) {
   req.url = req.url + '.gz';
   res.set('Content-Encoding', 'gzip');
   next();
 });
 
-// app.get('*.css', function(req, res) {
-//   res.set('Content-Type', 'text/css');
-//   res.sendFile(path.join(__dirname, '../src/style.css'));
-// });
+server.use(express.static(path.join(__dirname, '../build')));
 
-app.use(express.static(path.join(__dirname, '../build')));
+server.use('/', routes(db));
 
-app.use('/', routes);
-
-app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
 
-app.get('*', (req, res) => {
+server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
-
-// module.exports = {
-//   app
-// };
-
-
-const START, END;
-
-for (let START = 0; START < END; START++) {
-  const element = array[START];
-  
-}
