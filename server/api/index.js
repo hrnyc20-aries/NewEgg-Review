9a6d7e5650092fb13e0a95b1f9ede922da6808a3
Grahn Cooledge <gcooled29@gmail.com>
2019-03-07 12:39:42 -0500

    Remove Cluster

--- server/api/index.js
+++ server/api/index.js
@@ -1,33 +1,24 @@
 const express = require('express');
 const requests = require('./requestHandler');
 const seed = require('../database/mongoSeed');
-const cluster = require('../database/cluster');
 const dbToUSe = process.env.DATABASE_TO_USE || 'mongo';
 
 module.exports = (client) => {
   const router = express.Router();
 
   router.get('/reviews/:item_id', (req, res) => {
-    requests['GET'][dbToUSe](req, res);
+    requests['GET'][dbToUSe](req, res, client);
   });
 
   router.post('/reviews', (req, res) => {
-    requests['POST'][dbToUSe](req, res);
+    requests['POST'][dbToUSe](req, res, client);
   });
 
-  router.patch('/reviews/:review_id', (req, res) => {
-    requests['PATCH'][dbToUSe](req, res);
+  router.patch('/reviews/:review_id', (req, res, client) => {
+    requests['PATCH'][dbToUSe](req, res, client);
   });
 
-  router.put('/db/populate/seed/true/:limit', async (req, res) => {
-    // await seed(db, reviews);
-    // res.send({ Express: 'Seeded' });
-    let { limit } = req.params;
-
-    cluster(limit, res, (res) => {
-      res.send({ EXPRESS: 'Made it' });
-    });
-  });
+  // router.put('/db/populate/seed/true/:amount', seed);
 
   return router;
 };
